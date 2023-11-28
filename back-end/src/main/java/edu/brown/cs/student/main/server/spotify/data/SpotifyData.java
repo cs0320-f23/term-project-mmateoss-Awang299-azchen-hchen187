910c1064d2c3a168a824c653bfd232928e9f1c13
MarcelMatsal <marcel_mateos_salles@brown.edu>
2023-11-27 23:46:56 -0500

    chore: created the mocked json files that we will use to test the backend mimicking responses from the spotify api. Will allow us to test and work before it is all connected to the frontend

---
+++ back-end/src/main/java/edu/brown/cs/student/main/server/spotify/data/SpotifyData.java
@@ -0,0 +1,15 @@
+package edu.brown.cs.student.main.server.spotify.data;
+
+/**
+ * Class that will be used to get the needed data from the spotify API, allowing our
+ * server to use it accordingly.
+ */
+public class SpotifyData {
+
+  /**
+   * Constructor for the SpotifyData class.
+   */
+  public SpotifyData(){
+
+  }
+}
