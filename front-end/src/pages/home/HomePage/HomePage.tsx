e9103c794d60079d851982a1b5c47ecf15f1d5d4
Charlene Chen <hsuan-ling_chen@brown.edu>
2023-11-28 15:00:45 -0500

    feat: added HomePage and moved components over

---
+++ front-end/src/pages/home/HomePage/HomePage.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import './HomePage.css';
+import { LineAnimationComponent } from './HomePageComponents/LineAnimationComponent';
+import PersonComponent from './HomePageComponents/PersonComponent';
+import Toolbar from '../../../components/Toolbar/Toolbar';
+
+
+
+function HomePage() {
+    const navigate = useNavigate();
+    const [animate, setAnimate] = useState<boolean>(false);
+    const [headClicked, setHeadClicked] = useState<boolean>(false);
+    const transition = {duration: 1, ease: [0.43, 0.13, 0.23, 0.96]}
+    
+    const handleHeadClick = () => {
+      setAnimate(true);
+      setTimeout(() => {
+          setAnimate(false);
+          setHeadClicked(true);
+          setTimeout(() => {
+            navigate('/SongsPage');
+          }, 1000);
+      }, 1000);
+    }
+  
+    const animateStyle = {
+      scale: headClicked ? 50 : 1,
+      transformOrigin: '50% 25%',
+    };
+  
+  
+    return (
+        <body>
+          {/* //TODO: fix this animate/ exit issue */}
+          <motion.div exit={{opacity : 0}} transition={transition} className="main-container">
+            <Toolbar />
+            <div className="filler-container"></div>
+            <LineAnimationComponent startAnimation={animate} />
+            <div className="person-container">
+              {/* //TODO: same issue with this one */}
+                <motion.div key="text-container" exit={{opacity : 0}} transition={transition} className="text-container">
+                  <div className="text-bottom">
+                    this is a super cool website
+                    <br></br>
+                    blah blah blah blah blah balh blah blah blah 
+                    <br></br>
+                    yeet
+                  </div>
+                  {/* <div className="text-bottom">
+                    a different bit of text to 
+                    <br></br>
+                    make this website even cooler lolllllllllll 
+                    <br></br>
+                    yeehaw
+                  </div> */}
+                </motion.div>
+              <motion.div
+                initial={{ scale: 1 }}
+                transition={{ duration: 0.75 }}
+                animate={animateStyle}
+                className="expanded-container"
+              >
+              <PersonComponent handleHeadClick={handleHeadClick} headClicked={headClicked}/>
+              </motion.div>
+            </div>
+          </motion.div>
+        </body>
+    );
+  }
+  
+  export default HomePage;
---
+++ front-end/src/pages/home/HomePage/HomePageComponents/EyeComponent.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect} from 'react';
+import './../HomePage.css';
+
+interface EyeComponentProps {
+  headClicked: boolean;
+}
+
+function EyeComponent(props: EyeComponentProps) {
+  const [eyePosition, setEyePosition] = useState({ x: 200, y: 200 });
+  const [pupilPosition, setPupilPosition] = useState({ x: 0, y: 0 });
+  
+
+  useEffect(() => {
+    const container = document.querySelector('.head-container') as HTMLElement;
+    const eye = document.querySelector('.eye') as HTMLElement;
+    const pupil = document.querySelector('.pupil') as HTMLElement;
+
+    if (container && eye && pupil) {
+      document.addEventListener('mousemove', updateLocation);
+      return () => {
+        document.removeEventListener('mousemove', updateLocation);
+      };
+    }
+  }, []);
+
+  const updateLocation = (e: { clientX: number; clientY: number; }) => {
+    const container = document.querySelector('.head-container') as HTMLElement;
+    const eye = document.querySelector('.eye') as HTMLElement;
+
+    const containerRect = container.getBoundingClientRect();
+    const scaleX = containerRect.width / window.innerWidth*0.2;
+    const scaleY = containerRect.height / window.innerHeight*0.3;
+    const scaledX = (e.clientX - (containerRect.left + containerRect.width/2)) * scaleX;
+    const scaledY = (e.clientY - (containerRect.top + containerRect.height/2)) * scaleY;
+    setEyePosition({x: scaledX, y: scaledY});
+
+      
+    const containerEye = eye.getBoundingClientRect(); // Use the eye as the container
+    const scalePupilX = containerEye.width / window.innerWidth*0.5;
+    const scalePupilY = containerEye.height / window.innerHeight*0.5;
+    const scaledPupilX = (e.clientX - (containerEye.left + containerEye.width/2)) * scalePupilX;
+    const scaledPupilY = (e.clientY - (containerEye.top + containerEye.height/2)) * scalePupilY;
+    setPupilPosition({ x: scaledPupilX, y: scaledPupilY });
+  }
+
+  return (
+    <div className="eye" style={props.headClicked ? {} : {left: `${eyePosition.x}px`, top: `${eyePosition.y}px`}}>
+      <div style= {props.headClicked ? {} : {left: `${pupilPosition.x}px`, top: `${pupilPosition.y}px`}} className="pupil"></div>
+    </div>
+  );
+}
+
+export default EyeComponent;
