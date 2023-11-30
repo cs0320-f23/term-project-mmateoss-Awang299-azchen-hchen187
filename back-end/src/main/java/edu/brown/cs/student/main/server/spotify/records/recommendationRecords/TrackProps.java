4f35e855feea5c40e2cdd666f54e1ea990fd9ad0
MarcelMatsal <marcel_mateos_salles@brown.edu>
2023-11-30 14:14:57 -0500

    test: finished testing all the methods that make requests to the spotify api, they all seem to be working so can continue to work on the logic behind creating the parameters for the recommendation

--- back-end/src/main/java/edu/brown/cs/student/main/server/spotify/records/recommendationRecords/TrackProps.java
+++ back-end/src/main/java/edu/brown/cs/student/main/server/spotify/records/recommendationRecords/TrackProps.java
@@ -12,14 +12,13 @@ package edu.brown.cs.student.main.server.spotify.records.recommendationRecords;
  *                 ( true = yes it does; false = no it does not OR unknown).
  * @param href A link to the Web API endpoint providing full details of the track.
  * @param id The Spotify ID for the track.
- * @param is_playable  If true, the track is playable in the given market. Otherwise, false.
  * @param uri The Spotify URI for the track.
  * @param track_number The number of the track. If an album has several discs,
  *                     the track number is the number on the specified disc.
  * @param preview_url A link to a 30-second preview (MP3 format) of the track. Can be null
  * @param name The name of the track.
  */
-public record TrackProps(int duration_ms, boolean explicit, String href, String id, boolean
-    is_playable, String uri, int track_number, String preview_url, String name) {
+public record TrackProps(int duration_ms, boolean explicit, String href, String id,
+                         String uri, int track_number, String preview_url, String name) {
 
 }
--- back-end/src/test/java/edu/brown/cs/student/Tests/server/spotify/SpotifyRequestsTests.java
+++ back-end/src/test/java/edu/brown/cs/student/Tests/server/spotify/SpotifyRequestsTests.java
@@ -2,6 +2,8 @@ package edu.brown.cs.student.Tests.server.spotify;
 
 import edu.brown.cs.student.Tests.server.spotify.tokens.TestTokenGenerator;
 import edu.brown.cs.student.main.server.spotify.data.SpotifyData;
+import edu.brown.cs.student.main.server.spotify.records.audioFeaturesRecords.FeaturesProp;
+import edu.brown.cs.student.main.server.spotify.records.recommendationRecords.Recommendation;
 import edu.brown.cs.student.main.server.spotify.records.searchRecords.Song;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -10,15 +12,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testng.Assert;
 
+/**
+ * Class that test that we can make correct requests to the spotify API
+ */
 public class SpotifyRequestsTests {
 
   private String token;
   private SpotifyData data;
 
+  /**
+   * Constructor for the class
+   */
   public SpotifyRequestsTests(){
 
   }
 
+  /**
+   * Method called before each test is run
+   */
   @BeforeEach
   public void beforeTest(){
     TestTokenGenerator generator = new TestTokenGenerator();
@@ -46,6 +57,20 @@ public class SpotifyRequestsTests {
   @Test
   public void testGetFeatures() throws URISyntaxException, IOException,
       InterruptedException{
+    // all the song names
+    String[] names = new String[3];
+    names[0] = "I Will Wait";
+    //1fXiYSWmkKJfRKMegCkI11
+    names[1] = "Lucid Dreams";
+    //285pBltuF7vW8TeWk8hdRR
+    names[2] = "Levels";
+    //5UqCQaDshqbIk3pkhy4Pjg
+
+    FeaturesProp features = this.data.getFeatures(this.token, names);
+
+    Assert.assertEquals(features.audio_features().get(0).valence(), Float.valueOf("0.452"));
+    Assert.assertEquals(features.audio_features().get(1).danceability(), Float.valueOf("0.511"));
+    Assert.assertEquals(features.audio_features().get(2).liveness(), Float.valueOf("0.309"));
 
   }
 
@@ -56,6 +81,20 @@ public class SpotifyRequestsTests {
   @Test
   public void testGetRecommendation() throws URISyntaxException, IOException,
       InterruptedException{
+
+    String seedTrack = "1fXiYSWmkKJfRKMegCkI11";
+    Recommendation rec = this.data.getRecommendation(this.token,"3",seedTrack,
+        "0.2", "1","0.4","1",
+        "0.6", "1","0","1","0.5",
+        "1" );
+
+
+    Assert.assertTrue(rec.tracks().get(0).name() != null);
+    Assert.assertTrue(rec.tracks().get(1).id() != null);
+    Assert.assertTrue(rec.tracks().get(2).duration_ms() != 0);
+    Assert.assertTrue(rec.seeds().get(0).afterFilteringSize() >= 0);
+    Assert.assertEquals(rec.tracks().size(), 3);
+    Assert.assertEquals(rec.seeds().size(), 1);
 
   }
 
