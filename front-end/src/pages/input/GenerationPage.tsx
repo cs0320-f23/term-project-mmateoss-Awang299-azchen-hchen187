64360a3b06716ad48b3f488d95aaa89d0f3933d2
Allen Wang <allen_wang1@brown.edu>
2023-12-01 00:33:59 -0500

    feat: barebones generated-playlist-page + generation-page

--- front-end/src/pages/input/GenerationPage.tsx
+++ front-end/src/pages/input/GenerationPage.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './GenerationPage.css'
+import ProgressBar from '../../components/progress/ProgressBar'
 
 export default function GenerationPage() {
+
+  const [selectedSongs, setSelectedSongs] = useState(["song1"])
+  const [selectedMetaData, setSelectedMetaData] = useState(["metadata1"])
+  const [name, setName] = useState("")
+  const [length, setLength] = useState(0)
+
+  const handleGenerateButtonClick = () => {
+
+  }
+
   return (
-    <div style={{ color: "white"}}>GenerationPage</div>
-  )
+    <div className="generation-page">
+      <ProgressBar step={3} />
+      <div className="selections">
+        <div className="song-selections"></div>
+        {selectedSongs.map((song) => song)}
+        <div className="metadata-selections">{selectedMetaData.map((song) => song)}</div>
+      </div>
+      <div className="playlist-info">
+        <div className="name">
+          {" "}
+          <input placeholder="name"></input>
+        </div>
+        <div className="length">
+          {" "}
+          <input placeholder="length"></input>
+        </div>
+      </div>
+      <a href="/result">
+        <button className="generate-button"> GENERATE </button>
+      </a>
+    </div>
+  );
 }
--- front-end/src/pages/result/GeneratedPlaylistPage.tsx
+++ front-end/src/pages/result/GeneratedPlaylistPage.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { Paper } from '@mui/material'
+import React, { useState } from 'react'
+import "./GeneratedPlaylistPage.css"
 
 export default function GeneratedPlaylistPage() {
+  const [songs, setSongs] = useState(["song1", "song2", "song3"])
+
   return (
-    <div>GeneratedPlaylistPage</div>
-  )
+    <div className="generated-playlist-page">
+      {songs.map((song) => (
+        <Paper className="song-paper" elevation={3}>{song}</Paper>
+      ))}
+    </div>
+  );
 }
