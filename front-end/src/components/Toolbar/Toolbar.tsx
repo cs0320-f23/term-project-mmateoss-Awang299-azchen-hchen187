e2325fb4d9b7ef8343713db7a6066d9f46ed4dec
Charlene Chen <hsuan-ling_chen@brown.edu>
2023-12-04 13:10:43 -0500

    feat: changed toolbar style

--- front-end/src/components/Toolbar/Toolbar.tsx
+++ front-end/src/components/Toolbar/Toolbar.tsx
@@ -6,7 +6,7 @@ function Toolbar() {
 
     return (
     <header className="header">
-        <a href="/" className="logo">Logo</a>
+        <a href="/" className="logo">Spotimatch</a>
 
             <nav className="navbar">
                 <a href="/">Home</a>
