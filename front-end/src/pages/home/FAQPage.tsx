97d1ba88e0ccc8d127e2fc09f6725b6ef6608aeb
Charlene Chen <hsuan-ling_chen@brown.edu>
2023-12-15 21:24:47 -0500

    feat: started adding to faq page

--- front-end/src/pages/home/FAQPage.tsx
+++ front-end/src/pages/home/FAQPage.tsx
@@ -8,7 +8,19 @@ export default function FAQPage() {
     <div className="faq-page">
         <div className="main-container">
             <div className="overview-container">
-                How to play:
+                Overview of how it works:
+                <br/>
+                Input the song you want to learn
+                <br/>
+                Specify the language you want to learn in the song chosen
+                <br/>
+                Select the language you are most comfortable with {'(your native language)'}
+                <br/>
+                Select the level of difficulty you want your game to be
+                <br/>
+                Play game! The song audio will be played and you just have to fill in the
+                <br/>
+                missing words/line{'(s)'}. If you are correct, 
             </div>
         </div>
     </div>
