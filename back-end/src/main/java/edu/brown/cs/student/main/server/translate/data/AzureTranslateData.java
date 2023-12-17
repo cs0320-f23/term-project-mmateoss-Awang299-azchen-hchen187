2dd83295c071c552ba5054bea756d77187d797b1
Andrew Chen <andrewchen@andrewsmbp2155.devices.brown.edu>
2023-12-17 13:55:42 -0500

    azuretranslatedara minor fixe

--- back-end/src/main/java/edu/brown/cs/student/main/server/translate/data/AzureTranslateData.java
+++ back-end/src/main/java/edu/brown/cs/student/main/server/translate/data/AzureTranslateData.java
@@ -7,7 +7,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
-import java.util.Map;
 
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
